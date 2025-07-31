98d2d537db72b24f3eb1f41824aa5e113cd99dfc
Samruddhi2405 <samruddhinarkhede7293@gmail.com>
2025-07-31 22:02:00 +0530

    add forms and url

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
+from .utils import get_sentiment
+from .models import Feedback
+
+def home(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.sentiment = get_sentiment(feedback.text)
+            feedback.save()
+            return redirect('thank_you')
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/home.html', {'form': form})
+
+def thank_you(request):
+    return render(request, 'feedback/thank_you.html')
