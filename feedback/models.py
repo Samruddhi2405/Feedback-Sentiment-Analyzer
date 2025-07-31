8501d2c956a7cc506b60489ea82da38b655eeca8
Samruddhi2405 <samruddhinarkhede7293@gmail.com>
2025-07-31 21:49:17 +0530

    innitial commit

---
+++ feedback/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Feedback(models.Model):
+    text = models.TextField()
+    sentiment = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.text[:30]}... ({self.sentiment})"
